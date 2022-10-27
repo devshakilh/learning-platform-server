78eb5439704f200a6a672c3ac6ca95bb37ae7153
shakilla1 <shakilshifat65@gmail.com>
2022-10-27 23:06:10 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+app.use(cors());
+
+const categories = require('./data/category.json');
+const course = require('./data/course.json');
+
+app.get('/', (req, res) => {
+    res.send('Course API Running')
+});
